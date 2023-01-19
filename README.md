710c29bb08f9345c8a725239f5c6b9761a286954
NurulHelda <nurul.helda.tif20@gmail.com>
2023-01-19 11:03:48 +0700

    Technical Test SE

---
+++ README.md
@@ -0,0 +1,3 @@
+
+## Link
+* [Link screenshot dan record](https://drive.google.com/drive/folders/1SnyD5ruDSDggZScelXC7dmYgWc_DchUO?usp=share_link)
---
+++ Test 3/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#role-text', {
+    strings: ['Application', 'Mobile', 'Website'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100
+});
